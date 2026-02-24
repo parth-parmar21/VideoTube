870f981dba7981d85deb9027b1ab04aa763ee99c
parth-parmar21 <parthparmar3663@gmail.com>
2026-02-24 20:37:43 +0530

    write get user tweets

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -20,7 +20,6 @@ const createTweet = asyncHandler(async (req, res) => {
     if (!tweet) {
         throw new ApiError(404, "Failed to create tweet, please try again")
     }
-    console.log(tweet);
 
     return res
         .status(200)
@@ -29,7 +28,89 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const { userId } = req.params
+    
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "User id not found")
+    }
 
+    const tweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {   
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "tweet",
+                as: "likeDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            likedBy: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                likesCount: {
+                    $size: "$likeDetails"
+                },
+                ownerDetails: {
+                    $first: "$ownerDetails"
+                },
+                isLiked: {
+                    $cond: {
+                        if: {
+                            $in: [
+                                req.user?._id, "$likeDetails.likedBy"
+                            ]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                ownerDetails: 1,
+                likesCount: 1,
+                createdAt: 1,
+                isLiked: 1
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "Tweets fetched successfully"))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
@@ -37,17 +118,13 @@ const updateTweet = asyncHandler(async (req, res) => {
     const { content } = req.body
     const { tweetId } = req.params
 
-    console.log("content", content);
-
     if (!content) {
         throw new ApiError(400, "Content not found")
     }
-    console.log(content);
 
     if (!isValidObjectId(tweetId)) {
         throw new ApiError(400, "Invalid tweet Id")
     }
-    console.log(tweetId);
 
     const tweet = await Tweet.findById(tweetId)
 
