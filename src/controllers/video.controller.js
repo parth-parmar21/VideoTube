2f61dbcb30d4ab0fc7e0b9d85500715c1df47881
parth-parmar21 <parthparmar3663@gmail.com>
2026-03-01 14:55:25 +0530

    write a functionality for get all videos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,11 +5,94 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { text } from "express"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    const pipeline = []
+
+    if (query) {
+        pipeline.push({
+            $search: {
+                index: "search-videos",
+                text: {
+                    query: query,
+                    path: ['title', 'description']
+                }
+            }
+        })
+    }
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "User id not found")
+    }
+
+    pipeline.push({
+        $match: {
+            owner: new mongoose.Types.ObjectId(userId)
+        }
+    })
+
+
+    // fetch videos only their flag isPublished is true
+    pipeline.push({
+        $match: {
+            isPublished: true
+        }
+    })
+
+    // sortBy can be views, createdAt, duration
+    // sortType can be ascending(-1) or descending(1)
+
+    if (sortBy && sortType) {
+        pipeline.push({
+            $sort: {
+                [sortBy]: sortType === "asc" ? 1 : -1
+            }
+        })
+    } else{
+        pipeline.push({
+            $sort: {
+                createdAt: -1
+            }
+        })
+    }
+
+    pipeline.push(
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1
+                        }
+                    }
+                ]
+            }
+        },
+            {
+                $unwind: "$ownerDetails"
+            }
+        )
+
+        const videoAggregate = await Video.aggregate(pipeline)
+
+        const options = {
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10)
+        }
+
+        const video = await Video.aggregatePaginate(videoAggregate, options)
+    return res
+    .status(200)
+        .json(new ApiResponse(200, video, "Videos fetched successfully"))
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
