ed88f59404ed0e13808b3f5e804787424b5d9d85
parth-parmar21 <parthparmar3663@gmail.com>
2026-03-01 13:12:33 +0530

    write a functionality to get liked videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {Video} from '../models/video.model.js'
+import { title } from "process"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
@@ -99,6 +100,68 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideos",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails"
+                        }
+                    },
+                    {
+                        $unwind: "$ownerDetails",
+                    },
+                ],
+            },
+        },
+        {
+            $unwind: "$likedVideos"
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                likedVideo: {
+                    _id: "$likedVideos._id",
+                    videoFile: "$likedVideos.videoFile",
+                    thumbnail: "$likedVideos.thumbnail",
+                    title: "$likedVideos.title",
+                    description: "$likedVideos.description",
+                    isPublished: "$likedVideos.isPublished",
+                    createdAt: "$likedVideos.createdAt",
+                    views: "$likedVideos.views",
+                    duration: "$likedVideos.duration",
+                    owner: "$likedVideos.owner",
+                    ownerDetails: {
+                        fullName: "$likedVideos.ownerDetails.fullName",
+                        username: "$likedVideos.ownerDetails.username",
+                        avatar: "$likedVideos.ownerDetails.avatar"
+                    }
+                }
+            }
+        }
+    ])    
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, likedVideos, "Liked videos fetched succesfully"))
 })
 
 export {
