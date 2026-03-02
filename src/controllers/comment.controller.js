3a959ac4bf9ca7aee81b921687e38b136f7eb93c
parth-parmar21 <parthparmar3663@gmail.com>
2026-03-02 16:59:20 +0530

    implement getVideoComments functionality with pagination and user interaction details

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,15 +1,97 @@
-import mongoose, { isValidObjectId } from "mongoose"
+import mongoose, { get, isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Video id not found")
+    }
 
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(400, "Video not found")
+    }
+
+    const getComments = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner"
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "likes"
+            }
+        },
+        {
+            $addFields: {
+                likesCount: {
+                    $size: "$likes"
+                },
+                owner: {
+                    $first: "$owner"
+                },
+                isLiked: {
+                    $cond: {
+                        if: {
+                            $in: [ req.user?._id, "$likes.likedBy" ]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                createdAt: 1,
+                likesCount: 1,
+                owner: {
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1
+                },
+                isLiked: 1
+            }
+        }
+    ])
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10)
+    }
+
+    const comments = await Comment.aggregatePaginate(getComments, options)
+    console.log(comments);
+    
+    return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments fetched successfully"))
 })
 
 const addComment = asyncHandler(async (req, res) => {
